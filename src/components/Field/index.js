e8f9a3acee777dce284a4c812afff74be88ee474
Steve Mckellar <steve@wwqrd.uk>
2017-06-23 10:05:25 +0100

    Removed redundant field components

--- src/components/Field/index.js
+++
@@ -1,5 +0,0 @@
-export { default as Numeric } from './Numeric';
-export { default as Alphanumeric } from './Alphanumeric';
-export { default as CheckboxList } from './CheckboxList';
-export { default as CheckboxGroup } from './CheckboxGroup';
-export { default as ToggleGroup } from './ToggleGroup';
