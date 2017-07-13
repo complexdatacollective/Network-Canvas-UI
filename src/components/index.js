1c37145bed75cfcc839f4a2e1e9713bc887ec2fc
Stephen Mckellar <steve@wwqrd.uk>
2017-07-13 11:13:46 +0100

    Added narrative panel

--- src/components/index.js
+++ src/components/index.js
@@ -3,4 +3,6 @@ export Icon from './Icon';
 export TextInput from './Inputs/TextInput';
 export SideMenu from './SideMenu';
 export Node from './Node';
+export Expandable from './Expandable';
+export NarrativePanel from './NarrativePanel';
 export animation from './animation';
