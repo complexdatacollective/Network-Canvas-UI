ec2579dea0735328025ab1eb42d6a096481bd76c
Joshua Melville <joshua.melville@northwestern.edu>
2017-10-23 15:03:26 +0100

    add arrow icons

--- src/assets/img/icons/index.js
+++ src/assets/img/icons/index.js
@@ -3,6 +3,8 @@ import addAPerson from './add-a-person.svg.react.js';
 import addAPlace from './add-a-place.svg.react.js';
 import addARelationship from './add-a-relationship.svg.react.js';
 import addAScreen from './add-a-screen.svg.react.js';
+import arrowLeft from './arrow-left.svg.react.js';
+import arrowRight from './arrow-right.svg.react.js';
 import backArrow from './back-arrow.svg.react.js';
 import cancel from './cancel.svg.react.js';
 import close from './close.svg.react.js';
@@ -41,6 +43,8 @@ export default {
   'add-a-place': addAPlace,
   'add-a-relationship': addARelationship,
   'add-a-screen': addAScreen,
+  'arrow-left': arrowLeft,
+  'arrow-right': arrowRight,
   'back-arrow': backArrow,
   'cancel': cancel,
   'close': close,
