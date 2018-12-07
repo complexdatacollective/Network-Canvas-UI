61edecf0a65729d0526e49f9528682e7d281c258
Joshua Melville <joshua.melville@northwestern.edu>
2018-12-07 14:25:13 +0000

    add missing mock css variables

--- utils/__mocks__/CSSVariables.js
+++ utils/__mocks__/CSSVariables.js
@@ -5,8 +5,17 @@ const mockCSSVariables = {
   '--color-mustard': '#117733',
   '--color-sea-green': '#223344',
   '--white': '#fff',
-  '--animation-duration-slow-ms': '300',
-  '--animation-duration-standard-ms': '100',
+  '--ring': '#995522',
+  '--background': '#227733',
+  '--animation-scale-factor': 1,
+  '--animation-duration-fast': `${(1 * 0.3)}s`,
+  '--animation-duration-fast-ms': 1000 * 0.3,
+  '--animation-duration-standard': `${(1 * 0.5)}s`,
+  '--animation-duration-standard-ms': 1000 * 0.5,
+  '--animation-duration-slow': `${(1 * 1)}s`,
+  '--animation-duration-slow-ms': 1000 * 1,
+  '--animation-easing': 'cubic-bezier(0.4, 0, 0.2, 1)',
+  '--animation-easing-js': '[0.4, 0, 0.2, 1]',
 };
 
 const getCSSVariable = (cssVariableName) => {
