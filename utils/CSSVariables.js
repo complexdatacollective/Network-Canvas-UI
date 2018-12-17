cbc8ab64ffaf0b2f8d30878cb3420dd4685a6818
Joshua Melville <joshua.melville@northwestern.edu>
2018-12-17 11:17:41 +0000

    fix linting errors

--- utils/CSSVariables.js
+++ utils/CSSVariables.js
@@ -4,9 +4,9 @@ const CSSVariable = (variableName) => {
   const variable = getComputedStyle(document.body)
     .getPropertyValue(variableName)
     .trim();
-  // eslint-disable-next-line no-console
-  if (isEmpty(variable)) { 
-    console.log(`CSS variable "${variableName}" not found.`); 
+  if (isEmpty(variable)) {
+    // eslint-disable-next-line no-console
+    console.log(`CSS variable "${variableName}" not found.`);
     return null;
   }
   return variable;
