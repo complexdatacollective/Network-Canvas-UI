fc5a2c52893500c5b5a5f5b6d18c88c60082c765
Steve Mckellar <steve@wwqrd.uk>
2018-12-17 15:35:27 +0000

    Number input returns integer

--- components/Fields/Number.js
+++ components/Fields/Number.js
@@ -1,12 +1,15 @@
-import React, { PureComponent } from 'react';
+import { withProps, compose } from 'recompose';
 import TextInput from './Text';
 
-class NumberInput extends PureComponent {
-  render() {
-    return (
-      <TextInput type="number" {...this.props} />
-    );
-  }
-}
+const withNumericChangeHandler = withProps(props => ({
+  type: 'number',
+  input: {
+    ...props.input,
+    onChange: e => props.input.onChange(parseInt(e.target.value, 10)),
+    onBlur: e => props.input.onBlur(parseInt(e.target.value, 10)),
+  },
+}));
 
-export default NumberInput;
+export default compose(
+  withNumericChangeHandler,
+)(TextInput);
