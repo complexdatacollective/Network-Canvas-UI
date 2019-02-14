d098ddbb6dec3a9437e1b5c5c221ecf937287cfd
Steve Mckellar <steve@wwqrd.uk>
2019-02-14 12:54:01 +0000

    Rename Basic dialog to Simple dialog

--- components/Dialog/Simple.js
+++ components/Dialog/Simple.js
@@ -2,13 +2,16 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Modal from '../Modal';
 
-const BasicDialog = ({ children, show, options, title, onBlur, zIndex  }) => {
+/**
+ * A relatively unstyled dialog for use in other kinds of modals
+ */
+const SimpleDialog = ({ children, show, options, title, onBlur, zIndex  }) => {
   const styles = zIndex && { zIndex };
 
   return (
     <Modal show={show} onBlur={onBlur}>
       <div
-        className="dialog dialog--generic"
+        className="dialog dialog--simple"
         style={styles}
       >
         <div className="dialog__main">
@@ -25,21 +28,23 @@ const BasicDialog = ({ children, show, options, title, onBlur, zIndex  }) => {
   );
 };
 
-BasicDialog.propTypes = {
+SimpleDialog.propTypes = {
   show: PropTypes.bool,
   title: PropTypes.string.isRequired,
   children: PropTypes.node,
   options: PropTypes.arrayOf(PropTypes.element),
   onBlur: PropTypes.func,
+  zIndex: PropTypes.number,
 };
 
-BasicDialog.defaultProps = {
+SimpleDialog.defaultProps = {
   show: false,
   children: null,
   options: [],
   onBlur: () => {},
+  zIndex: null,
 };
 
-export { BasicDialog };
+export { SimpleDialog };
 
-export default BasicDialog;
+export default SimpleDialog;
