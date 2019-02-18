b1e9ad2b49dc0542667703755596864c3b3362fa
Steve Mckellar <steve@wwqrd.uk>
2019-02-18 12:36:44 +0000

    Externalise simple dialog styles

--- components/Dialog/Simple.js
+++ components/Dialog/Simple.js
@@ -1,32 +1,37 @@
 import React from 'react';
+import cx from 'classnames';
 import PropTypes from 'prop-types';
 import Modal from '../Modal';
 
 /**
  * A relatively unstyled dialog for use in other kinds of modals
  */
-const SimpleDialog = ({ children, show, options, title, onBlur, zIndex  }) => {
-  const styles = zIndex && { zIndex };
-
-  return (
-    <Modal show={show} onBlur={onBlur}>
-      <div
-        className="dialog dialog--simple"
-        style={styles}
-      >
-        <div className="dialog__main">
-          <div className="dialog__main-content">
-            <h2 className="dialog__main-title">{title}</h2>
-            {children}
-          </div>
+const SimpleDialog = ({
+  children,
+  show,
+  options,
+  title,
+  onBlur,
+  className,
+  style,
+}) => (
+  <Modal show={show} onBlur={onBlur}>
+    <div
+      className={cx('dialog', 'dialog--simple', className)}
+      style={style}
+    >
+      <div className="dialog__main">
+        <div className="dialog__main-content">
+          <h2 className="dialog__main-title">{title}</h2>
+          {children}
         </div>
-        <footer className="dialog__footer">
-          {options}
-        </footer>
       </div>
-    </Modal>
-  );
-};
+      <footer className="dialog__footer">
+        {options}
+      </footer>
+    </div>
+  </Modal>
+);
 
 SimpleDialog.propTypes = {
   show: PropTypes.bool,
@@ -34,7 +39,8 @@ SimpleDialog.propTypes = {
   children: PropTypes.node,
   options: PropTypes.arrayOf(PropTypes.element),
   onBlur: PropTypes.func,
-  zIndex: PropTypes.number,
+  style: PropTypes.object,
+  className: PropTypes.oneOfType(PropTypes.string, PropTypes.object),
 };
 
 SimpleDialog.defaultProps = {
@@ -42,7 +48,8 @@ SimpleDialog.defaultProps = {
   children: null,
   options: [],
   onBlur: () => {},
-  zIndex: null,
+  style: {},
+  className: null,
 };
 
 export { SimpleDialog };
