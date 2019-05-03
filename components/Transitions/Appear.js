f9b1947f73e76843455be38eaf4c7935ef2a3e30
Joshua Melville <joshua.melville@northwestern.edu>
2019-05-03 13:32:13 +0100

    include server transitions

---
+++ components/Transitions/Appear.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Transition } from 'react-transition-group';
+import anime from 'animejs';
+import { getCSSVariableAsNumber, getCSSVariableAsObject } from '../../utils/CSSVariables';
+
+const defaultEasing = getCSSVariableAsObject('--animation-easing-js');
+
+const appear = () => ({
+  opacity: [0, 1],
+  scale: [0, 1],
+  elasticity: 0,
+  easing: defaultEasing,
+  duration: getCSSVariableAsNumber('--animation-duration-standard-ms'),
+});
+
+const disappear = () => ({
+  opacity: [1, 0],
+  scale: [1, 0],
+  height: 0,
+  margin: 0,
+  elasticity: 0,
+  easing: defaultEasing,
+  duration: getCSSVariableAsNumber('--animation-duration-standard-ms'),
+});
+
+const AppearTransition = ({ children, ...props }) => (
+  <Transition
+    mountOnEnter
+    unmountOnExit
+    timeout={getCSSVariableAsNumber('--animation-duration-standard-ms')}
+    onEntering={el => anime({ targets: el, ...appear() })}
+    onExiting={el => anime({ targets: el, ...disappear() })}
+    {...props}
+  >
+    { children }
+  </Transition>
+);
+
+AppearTransition.propTypes = {
+  children: PropTypes.any.isRequired,
+};
+
+AppearTransition.defaultProps = {
+  children: null,
+};
+
+export default AppearTransition;
--- components/Transitions/index.js
+++ components/Transitions/index.js
@@ -1,2 +1,5 @@
+export { default as Appear } from './Appear';
+export { default as Drawer } from './Drawer';
 export { default as Fade } from './Fade';
 export { default as Drop } from './Drop';
+export { default as Notify } from './Notify';
