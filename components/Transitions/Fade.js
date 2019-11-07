8041d7b7b2f719aea094903c5750151ffaa67917
Joshua Melville <joshua.melville@northwestern.edu>
2019-11-07 12:32:23 +0000

    port Architect transitions and update package.json node and react versions

--- components/Transitions/Fade.js
+++ components/Transitions/Fade.js
@@ -1,75 +1,55 @@
 import React, { PureComponent } from 'react';
-import PropTypes from 'prop-types';
 import { Transition } from 'react-transition-group';
 import anime from 'animejs';
 import { getCSSVariableAsNumber, getCSSVariableAsObject } from '../../utils/CSSVariables';
 
-class Fade extends PureComponent {
-  defaultDuration = {
+function Fade(props) {
+  const { children, customDuration, customEasing, enter, onExited } = props;
+
+  const defaultDuration = {
     enter: getCSSVariableAsNumber('--animation-duration-fast-ms'),
     exit: getCSSVariableAsNumber('--animation-duration-fast-ms'),
   };
 
-  defaultEasing = getCSSVariableAsObject('--animation-easing-js');
+  const defaultEasing = getCSSVariableAsObject('--animation-easing-js');
 
-  render() {
-    const { children, customDuration, customEasing, enter, onExited } = this.props;
+  const duration = customDuration || defaultDuration;
+  const easing = customEasing || defaultEasing;
 
-    const duration = customDuration || this.defaultDuration;
-    const easing = customEasing || this.defaultEasing;
-
-    return (
-      <Transition
-        timeout={duration}
-        onEnter={
-          (el) => {
-            anime({
-              targets: el,
-              opacity: [0, 1],
-              elasticity: 0,
-              easing,
-              duration: duration.enter,
-            });
-          }
+  return (
+    <Transition
+      timeout={duration}
+      onEnter={
+        (el) => {
+          anime({
+            targets: el,
+            opacity: [0, 1],
+            elasticity: 0,
+            easing,
+            duration: duration.enter,
+          });
         }
-        onExit={
-          (el) => {
-            anime({
-              targets: el,
-              opacity: [1, 0],
-              elasticity: 0,
-              easing,
-              duration: duration.exit,
-            });
-          }
+      }
+      onExit={
+        (el) => {
+          anime({
+            targets: el,
+            opacity: [1, 0],
+            elasticity: 0,
+            easing,
+            duration: duration.exit,
+          });
         }
-        enter={enter}
-        in={this.props.in}
-        appear
-        unmountOnExit
-        onExited={onExited}
-      >
-        { children }
-      </Transition>
-    );
-  }
+      }
+      enter={enter}
+      in={props.in}
+      appear
+      unmountOnExit
+      onExited={onExited}
+    >
+      { children }
+    </Transition>
+  );
 }
-
-Fade.propTypes = {
-  children: PropTypes.any,
-  customDuration: PropTypes.object,
-  customEasing: PropTypes.array,
-  enter: PropTypes.bool,
-  in: PropTypes.bool.isRequired,
-  onExited: PropTypes.func,
-};
-
-Fade.defaultProps = {
-  children: null,
-  customDuration: null,
-  customEasing: null,
-  enter: true,
-  onExited: () => { },
-};
 
 export default Fade;
--- components/Transitions/index.js
+++ components/Transitions/index.js
@@ -1,5 +1,8 @@
 export { default as Appear } from './Appear';
+export { default as None } from './None';
 export { default as Drawer } from './Drawer';
 export { default as Fade } from './Fade';
 export { default as Drop } from './Drop';
 export { default as Notify } from './Notify';
+export { default as TimelineScreen } from './TimelineScreen';
+export { default as Wipe } from './Wipe';
