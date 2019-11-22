885288562048fbd4da3d456874b1178969145be5
Joshua Melville <joshua.melville@northwestern.edu>
2019-11-22 12:28:04 +0000

    refactor Appear and Drawer to correctly use getCSSVariable inside function

--- components/Transitions/Appear.js
+++ components/Transitions/Appear.js
@@ -4,38 +4,41 @@ import { Transition } from 'react-transition-group';
 import anime from 'animejs';
 import { getCSSVariableAsNumber, getCSSVariableAsObject } from '../../utils/CSSVariables';
 
-const defaultEasing = getCSSVariableAsObject('--animation-easing-js');
+const AppearTransition = ({ children, ...props }) => {
 
-const appear = () => ({
-  opacity: [0, 1],
-  scale: [0, 1],
-  elasticity: 0,
-  easing: defaultEasing,
-  duration: getCSSVariableAsNumber('--animation-duration-standard-ms'),
-});
+  const defaultEasing = getCSSVariableAsObject('--animation-easing-js');
 
-const disappear = () => ({
-  opacity: [1, 0],
-  scale: [1, 0],
-  height: 0,
-  margin: 0,
-  elasticity: 0,
-  easing: defaultEasing,
-  duration: getCSSVariableAsNumber('--animation-duration-standard-ms'),
-});
+  const appear = () => ({
+    opacity: [0, 1],
+    scale: [0, 1],
+    elasticity: 0,
+    easing: defaultEasing,
+    duration: getCSSVariableAsNumber('--animation-duration-standard-ms'),
+  });
 
-const AppearTransition = ({ children, ...props }) => (
-  <Transition
-    mountOnEnter
-    unmountOnExit
-    timeout={getCSSVariableAsNumber('--animation-duration-standard-ms')}
-    onEntering={el => anime({ targets: el, ...appear() })}
-    onExiting={el => anime({ targets: el, ...disappear() })}
-    {...props}
-  >
-    { children }
-  </Transition>
-);
+  const disappear = () => ({
+    opacity: [1, 0],
+    scale: [1, 0],
+    height: 0,
+    margin: 0,
+    elasticity: 0,
+    easing: defaultEasing,
+    duration: getCSSVariableAsNumber('--animation-duration-standard-ms'),
+  });
+
+  return (
+    <Transition
+      mountOnEnter
+      unmountOnExit
+      timeout={getCSSVariableAsNumber('--animation-duration-standard-ms')}
+      onEntering={el => anime({ targets: el, ...appear() })}
+      onExiting={el => anime({ targets: el, ...disappear() })}
+      {...props}
+    >
+      { children }
+    </Transition>
+  );
+};
 
 AppearTransition.propTypes = {
   children: PropTypes.any.isRequired,
