b15dc6a4194cfbad53cdbea7496586809d705f5b
Steve Mckellar <steve@wwqrd.uk>
2020-09-02 16:25:21 +0100

    Update window HOC to use same portal for all components

--- src/components/window.js
+++ src/components/window.js
@@ -10,43 +10,18 @@ const getDisplayName = WrappedComponent =>
  * HOC which will cause a component to be rendered outside of the main ReactDOM hierachy,
  * useful for modals and other windowed components.
  */
-const window = WrappedComponent =>
+const window = (WrappedComponent, defaultRoot = document.body) =>
   class Window extends Component {
-    constructor(props) {
-      super(props);
-
-      this.portal = document.createElement('div');
-    }
-
-    componentDidMount() {
-      this.root.appendChild(this.portal);
-    }
-
-    componentDidUpdate(prevProps) {
-      if (prevProps.windowRoot && prevProps.windowRoot !== this.root) {
-        prevProps.windowRoot.removeChild(this.portal);
-        this.root.appendChild(this.portal);
-      }
-    }
-
-    componentWillUnmount() {
-      this.root.removeChild(this.portal);
-    }
-
-    get root() {
-      // If a root reference element is provided by windowRootConsumer() use that,
-      // otherwise default to document.body
-      return this.props.windowRoot || document.body;
-    }
-
     static get displayName() {
       return `Window(${getDisplayName(WrappedComponent)})`;
     }
 
     render() {
+      const portal = this.props.windowRoot || defaultRoot;
+
       return ReactDOM.createPortal(
         <WrappedComponent {...this.props} />,
-        this.portal,
+        portal,
       );
     }
   };
