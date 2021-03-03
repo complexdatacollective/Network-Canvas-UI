2d8693fe793302712a8f44a388ae213b61847876
Rebecca Madsen <sollasollew@outlook.com>
2021-03-03 12:00:31 -0500

    remove unused tags

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,23 +1,11 @@
-const ALLOWED_MARKDOWN_TAGS = [
-  'break',
-  'emphasis',
-  'heading',
-  'link',
-  'list',
-  'listItem',
-  'paragraph',
-  'strong',
-  'thematicBreak',
-];
-
 const ALLOWED_MARKDOWN_LABEL_TAGS = [
   'paragraph',
   'break',
   'emphasis',
   'strong',
+  'text',
 ];
 
 module.exports = {
-  ALLOWED_MARKDOWN_TAGS,
   ALLOWED_MARKDOWN_LABEL_TAGS,
 };
