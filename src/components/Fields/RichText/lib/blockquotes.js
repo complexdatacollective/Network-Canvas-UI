91b4d62c410514e221d05b933f6d5026d9d97363
Steve Mckellar <steve@wwqrd.uk>
2021-04-06 13:07:41 +0100

    Block quote toggling

--- src/components/Fields/RichText/lib/blockquotes.js
+++ src/components/Fields/RichText/lib/blockquotes.js
@@ -3,6 +3,7 @@ import {
   Editor,
   Range,
   Transforms,
+  Node,
 } from 'slate';
 import { get } from 'lodash';
 
@@ -20,7 +21,6 @@ const onBackspace = () => {
 const toggleBlockquote = (editor) => () => {
   // Toggle blockquote AND paragraph
   const { selection } = editor;
-
   const { anchor } = selection;
 
   const block = Editor.above(editor, {
@@ -29,17 +29,35 @@ const toggleBlockquote = (editor) => () => {
   });
 
   const type = get(block, [0, 'type']);
-  const path = block ? block[1] : [];
+  const path = get(block, [1], []);
 
   // de-blockquote
-  if (type === 'block_quote') {
-    Transforms.liftNodes(editor, path);
-  } else if (type === 'paragraph') {
-    Transforms.wrapNodes(
-      editor,
-      { type: 'block_quote' },
-      path,
-    );
+  switch (type) {
+    case 'block_quote':
+      Transforms.liftNodes(editor);
+      break;
+    case 'ul_list':
+    case 'ol_list':
+      // Unwrap all list items
+      Transforms.unwrapNodes(
+        editor,
+        {
+          at: path,
+          match: (n) => n.type === 'list_item',
+          mode: 'all',
+        },
+      );
+      // Set top level element to a block quote
+      Transforms.setNodes(editor, { type: 'block_quote' }, { at: path });
+      break;
+    case 'paragraph':
+      Transforms.wrapNodes(
+        editor,
+        { type: 'block_quote' },
+        { at: path },
+      );
+      break;
+    default:
   }
 };
 
