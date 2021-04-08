58ea8370b2f75dccdfcd98fec8e0729627557b99
Joshua Melville <joshua.melville@northwestern.edu>
2021-04-08 22:17:10 +0200

    correctly export Boolean component

--- src/components/Fields/index.js
+++ src/components/Fields/index.js
@@ -1,4 +1,5 @@
 /* eslint-disable import/prefer-default-export */
+export { default as Boolean } from './Boolean';
 export { default as Text } from './Text';
 export { default as Number } from './Number';
 export { default as TextArea } from './TextArea';
