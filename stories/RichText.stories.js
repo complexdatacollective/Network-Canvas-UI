1a978cc57d5ed5bf0c7010af8431add18144d8f8
Steve Mckellar <steve@wwqrd.uk>
2021-04-19 12:25:27 +0100

    Prevent inline option from being changed

--- stories/RichText.stories.js
+++ stories/RichText.stories.js
@@ -1,19 +1,11 @@
 /* eslint-disable react/jsx-props-no-spreading */
-import React, { useEffect, useState } from 'react';
-import { action } from '@storybook/addon-actions';
+import React, { useState } from 'react';
 import ReactMarkdown from 'react-markdown';
-import Harness from './helpers/Harness';
 import { Field as RichTextField } from '../src/components/Fields/RichText';
 import { ALLOWED_MARKDOWN_LABEL_TAGS } from '../src/utils/config';
 
 import '../src/styles/_all.scss';
 import MarkdownLabel from '../src/components/Fields/MarkdownLabel';
-
-// const requiredProps = {
-//   label: 'This a<br>b prompt text contains **markdown** _formatting_\n\n- Across lines\n- And others',
-//   input: { value:  },
-//   meta: {},
-// };
 
 const initialValue = '# Heading!\n\nsomething **with markdown**\n\n- and a\n- list\n\nThat has other elements following it.\n\n1. Numbered\n1. Elements\n\n> A block quote\n\nHello';
 
@@ -24,6 +16,7 @@ export default {
   },
   argTypes: {
     initialValue: { control: null },
+    inline: { control: null },
   },
 };
 
