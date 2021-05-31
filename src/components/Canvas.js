8b1045c0ac6b61a4c491ffee45a2c0d8960eed4b
Joshua Melville <joshua.melville@northwestern.edu>
2021-06-01 01:44:23 +0200

    quick cleanup

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -1,54 +1,25 @@
-/* eslint-disable react/prop-types */
-import React, { useEffect } from 'react';
-import useResizeAware from 'react-resize-aware';
+import React from 'react';
+import PropTypes from 'prop-types';
 import useCanvas from '../hooks/useCanvas';
 
-function resizeCanvas(canvas) {
-  const { width, height } = canvas.getBoundingClientRect();
-
-  if (canvas.width !== width || canvas.height !== height) {
-    const { devicePixelRatio: ratio = 1 } = window;
-    // console.log(ratio);
-    const context = canvas.getContext('2d');
-    canvas.width = width * ratio;
-    canvas.height = height * ratio;
-    context.scale(ratio, ratio);
-    return true;
-  }
-
-  return false;
-}
-
-const defaultPredraw = (context, canvas) => {
-  // console.log('defaultPredraw');
-  context.save();
-  resizeCanvas(canvas);
-  const { width, height } = context.canvas;
-  context.clearRect(0, 0, width, height);
-};
-
-const defaultPostdraw = (context) => {
-  // console.log('defaultPostdraw');
-  // index += 1;
-  context.restore();
-};
-
 const Canvas = (props) => {
-  const { draw, predraw = defaultPredraw, postdraw = defaultPostdraw, ...rest } = props;
-  const canvasRef = useCanvas(draw, { predraw, postdraw });
-
-  const [resizeListener, sizes] = useResizeAware();
-
-  useEffect(() => {
-    resizeCanvas(canvasRef.current);
-  }, [canvasRef, sizes]);
+  const { draw, predraw, postdraw } = props;
+  const canvasRef = useCanvas(draw, predraw, postdraw);
 
   return (
-    <div style={{ width: '100%', height: '100%' }}>
-      {resizeListener}
-      <canvas ref={canvasRef} {...rest} style={{ width: '100%', height: '100%' }} />
-    </div>
+    <canvas ref={canvasRef} style={{ width: '100%', height: '100%' }} />
   );
+};
+
+Canvas.propTypes = {
+  draw: PropTypes.func.isRequired,
+  predraw: PropTypes.func,
+  postdraw: PropTypes.func,
+};
+
+Canvas.defaultProps = {
+  predraw: undefined,
+  postdraw: undefined,
 };
 
 export default Canvas;
--- src/hooks/useCanvas.js
+++ src/hooks/useCanvas.js
@@ -1,6 +1,34 @@
 import { useRef, useEffect } from 'react';
 
-const useCanvas = (draw, { predraw, postdraw }) => {
+const resizeCanvas = (
+  context,
+  _,
+  { current: currentCanvas },
+) => { /* eslint-disable no-param-reassign */
+  const { width, height } = currentCanvas.getBoundingClientRect();
+  const { devicePixelRatio: ratio = 1 } = window;
+
+  if (currentCanvas.width !== width * ratio || currentCanvas.height !== height * ratio) {
+    currentCanvas.width = width * ratio;
+    currentCanvas.height = height * ratio;
+    context.scale(ratio, ratio);
+    return true;
+  }
+
+  return false;
+};
+
+const defaultPredraw = (context) => {
+  context.save();
+  const { width, height } = context.canvas;
+  context.clearRect(0, 0, width, height);
+};
+
+const defaultPostdraw = (context) => {
+  context.restore();
+};
+
+const useCanvas = (draw, predraw = defaultPredraw, postdraw = defaultPostdraw) => {
   const canvasRef = useRef(null);
 
   useEffect(() => {
@@ -8,10 +36,11 @@ const useCanvas = (draw, { predraw, postdraw }) => {
     const context = canvas.getContext('2d');
     let requestAnimationId;
     const render = (ctx, time) => {
-      predraw(ctx, canvasRef.current);
-      draw(ctx, time, canvasRef.current);
-      postdraw(ctx);
-      requestAnimationId = requestAnimationFrame((time) => render(ctx, time));
+      resizeCanvas(context, time, canvasRef);
+      predraw(ctx, time, canvasRef);
+      draw(ctx, time, canvasRef);
+      postdraw(ctx, time, canvasRef);
+      requestAnimationId = requestAnimationFrame((t) => render(ctx, t));
     };
     render(context);
 
