8c8246d1528011732227b0daee2552910f33025e
Joshua Melville <joshua.melville@northwestern.edu>
2021-06-02 15:20:15 +0200

    escape asterisk and underscore characters

--- src/components/Fields/RichText/lib/serialize.js
+++ src/components/Fields/RichText/lib/serialize.js
@@ -3,6 +3,8 @@ import { serialize } from 'remark-slate';
 const serializeNodes = (nodes) => (
   nodes.map((n) => serialize(n))
     .join('\n')
+    .replace(/\*/g, '\\*')
+    .replace(/_/g, '\\_')
     .replace(/\[/g, '\\[')
     .replace(/\]/g, '\\]')
 );
