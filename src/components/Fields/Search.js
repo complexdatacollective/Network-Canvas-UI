80a78cae512e753aa18114573562cf1ce0d69708
Steve Mckellar <steve@wwqrd.uk>
2021-06-03 15:00:38 +0100

    Get search icon color from css

--- src/components/Fields/Search.js
+++ src/components/Fields/Search.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import SearchIcon from '@material-ui/icons/SearchRounded';
+import { getCSSVariableAsString } from '../../utils/CSSVariables';
 import Text from './Text';
 
-const Search = (props) => (
-  <Text
-    adornmentPosition="right"
-    {...props} // eslint-disable-line react/jsx-props-no-spreading
-    type="search"
-    adornment={<SearchIcon style={{ color: '#fff' }} />}
-  />
-);
+const Search = (props) => {
+  const color = getCSSVariableAsString('--input-text');
+
+  return (
+    <Text
+      adornmentPosition="right"
+      {...props} // eslint-disable-line react/jsx-props-no-spreading
+      type="search"
+      adornment={color && <SearchIcon style={{ color }} />}
+    />
+  );
+};
 
 export default Search;
