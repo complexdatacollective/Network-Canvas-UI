191765f0a7edc6dd626d997d46166fa2f38bf116
Joshua Melville <joshua.melville@northwestern.edu>
2021-06-07 13:08:42 +0200

    port emoji support

--- src/components/Fields/Markdown.js
+++ src/components/Fields/Markdown.js
@@ -3,14 +3,38 @@ import ReactMarkdown from 'react-markdown';
 import rehypeRaw from 'rehype-raw';
 import rehypeSanitize from 'rehype-sanitize';
 import PropTypes from 'prop-types';
+import emoji from 'emoji-dictionary';
 import { ALLOWED_MARKDOWN_TAGS } from '../../utils/config';
 import { escapeAngleBracket } from './RichText/lib/parse';
+
+const emojiTextRenderer = ({ node, ...props }) => (
+  <p>
+    {props.children.map(child => {
+      if (typeof child === 'string') {
+        return child.replace(/:\w+:/gi, (name) => emoji.getUnicode(name));
+      }
+      return child;
+    })}
+  </p>
+);
+
+const externalLinkRenderer = ({ href, children }) => (
+  <a href={href} target="_blank" rel="noopener noreferrer">
+    {children}
+  </a>
+);
+
+const defaultMarkdownRenderers = {
+  '**': 'span',
+  p: emojiTextRenderer,
+  a: externalLinkRenderer,
+};
 
 const Markdown = ({ label, className, allowedElements = ALLOWED_MARKDOWN_TAGS }) => (
   <ReactMarkdown
     className={className}
     allowedElements={allowedElements}
-    components={{ root: 'span' }}
+    components={defaultMarkdownRenderers}
     rehypePlugins={[rehypeRaw, rehypeSanitize]}
     unwrapDisallowed
   >
