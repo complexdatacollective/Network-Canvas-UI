4837ae855ff3d9eb93bf57283019fc666fd8d065
Steve Mckellar <steve@wwqrd.uk>
2021-06-16 12:14:22 +0100

    Include clamped value in Scroller

--- stories/Scrollable.stories.js
+++ stories/Scrollable.stories.js
@@ -1,37 +1,29 @@
 import React from 'react';
 import faker from 'faker';
-import Harness from './helpers/Harness';
 import Scroller from '../src/components/Scroller';
 import '../src/styles/_all.scss';
 
-const requiredProps = {
-  children: [...new Array(20)].map((_, index) => (<p key={index}>{faker.lorem.paragraph(20)}</p>)),
+export default {
+  title: 'Utilities/Scroller',
+  args: {
+    children: [...new Array(20)]
+      .map((_, index) => (<p key={index}>{faker.lorem.paragraph(20)}</p>)),
+  },
+  argTypes: { onScroll: { action: 'scrolled' } },
 };
 
-export default { title: 'Utilities/Scroller' };
-
-export const renders = () => {
-  return (
-    <Harness
-      requiredProps={requiredProps}
-    >
-      {
-        props =>
-          (<Scroller {...props} />)
-      }
-    </Harness>
-  );
-};
-
-export const rendersHeading = () => (
-  <Harness
-    requiredProps={requiredProps}
-  >
-    {
-      props =>
-        (<h1><Scroller {...props}>
-          Eulogy for an Adolescence Shattered Against Elliot Street Pavement - Heres to Being Young!
-        </Scroller></h1>)
-    }
-  </Harness>
+export const renders = (props) => (
+  <div style={{ height: 600, display: 'flex' }}>
+    <Scroller {...props} />
+  </div>
 );
+renders.args = {};
+
+export const rendersHeading = (props) => (
+  <h1>
+    <Scroller {...props} />
+  </h1>
+);
+rendersHeading.args = {
+  children: 'Eulogy for an Adolescence Shattered Against Elliot Street Pavement - Heres to Being Young!',
+};
